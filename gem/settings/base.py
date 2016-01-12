63a04f0c6fb525ca2fe0d51a7ed48cbd4833aa68
Saeed Marzban <Saeed.mb8@gmail.com>
2016-01-12 09:43:26 +0200

    Scaffolded the gem app and installed the existing plugins

---
+++ gem/settings/base.py
@@ -0,0 +1,203 @@
+"""
+Django settings for base gem.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+from os.path import abspath, dirname, join
+from django.conf import global_settings
+from django.utils.translation import ugettext_lazy as _
+
+# Absolute filesystem path to the Django project directory:
+PROJECT_ROOT = dirname(dirname(dirname(abspath(__file__))))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "dqji)!xte^trgai!3c)_4)ftaoevwvbog-i&nl$#ef9xb+y*ab"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Base URL to use when referring to full URLs within the Wagtail admin
+# backend - e.g. in notification emails. Don't include '/admin' or
+# a trailing slash
+BASE_URL = 'http://example.com'
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'compressor',
+    'taggit',
+    'modelcluster',
+
+    'wagtail.wagtailcore',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailsites',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailredirects',
+    'wagtail.wagtailforms',
+
+    'molo.core',
+    'gem',
+    'molo.profiles',
+    'django_comments',
+    'django.contrib.sites',
+    'molo.commenting',
+    'molo.yourwords',
+
+    'raven.contrib.django.raven_compat',
+)
+
+COMMENTS_APP = 'molo.commenting'
+SITE_ID = 1
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'wagtail.wagtailcore.middleware.SiteMiddleware',
+    'wagtail.wagtailredirects.middleware.RedirectMiddleware',
+)
+
+ROOT_URLCONF = 'gem.urls'
+WSGI_APPLICATION = 'gem.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+# SQLite (simplest install)
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(
+    default='sqlite:///%s' % (join(PROJECT_ROOT, 'db.sqlite3'),))}
+
+# PostgreSQL (Recommended, but requires the psycopg2 library and Postgresql
+#             development headers)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'base',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': '',  # Set to empty string for localhost.
+#         'PORT': '',  # Set to empty string for default.
+#         # number of seconds database connections should persist for
+#         'CONN_MAX_AGE': 600,
+#     }
+# }
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-gb'
+TIME_ZONE = 'Africa/Johannesburg'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Native South African languages are currently not included in the default
+# list of languges in django
+# https://github.com/django/django/blob/master/django/conf/global_settings.py#L50
+LANGUAGES = global_settings.LANGUAGES + (
+    ('zu', _('Zulu')),
+    ('xh', _('Xhosa')),
+    ('st', _('Sotho')),
+    ('ve', _('Venda')),
+    ('tn', _('Tswana')),
+    ('ts', _('Tsonga')),
+    ('ss', _('Swati')),
+    ('nr', _('Ndebele')),
+)
+
+LOCALE_PATHS = (
+    join(PROJECT_ROOT, "locale"),
+)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_ROOT = join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+MEDIA_ROOT = join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+
+
+# Django compressor settings
+# http://django-compressor.readthedocs.org/en/latest/settings/
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+
+# Template configuration
+
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
+    'molo.core.context_processors.locale',
+)
+
+
+# Wagtail settings
+
+LOGIN_URL = 'wagtailadmin_login'
+LOGIN_REDIRECT_URL = 'wagtailadmin_home'
+
+WAGTAIL_SITE_NAME = "base"
+
+# Use Elasticsearch as the search backend for extra performance and better
+# search results:
+# http://wagtail.readthedocs.org/en/latest/howto/performance.html#search
+# http://wagtail.readthedocs.org/en/latest/core_components/
+#     search/backends.html#elasticsearch-backend
+#
+# WAGTAILSEARCH_BACKENDS = {
+#     'default': {
+#         'BACKEND': ('wagtail.wagtailsearch.backends.'
+#                     'elasticsearch.ElasticSearch'),
+#         'INDEX': 'base',
+#     },
+# }
+
+
+# Whether to use face/feature detection to improve image
+# cropping - requires OpenCV
+WAGTAILIMAGES_FEATURE_DETECTION_ENABLED = False
