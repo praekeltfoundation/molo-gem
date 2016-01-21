de4869b34857114f080104a11e56cf93a3741002
Colin Alston <colin.alston@gmail.com>
2016-01-21 10:33:25 +0200

    Use fqdn

--- sideloader/build.sh
+++ sideloader/build.sh
@@ -4,6 +4,6 @@ cd $REPO
 
 docker build -t gem-molo .
 
-docker tag gem-molo qa-mesos-persistence:5000/gem-molo
+docker tag gem-molo qa-mesos-persistence.za.prk-host.net:5000/gem-molo
 
-docker push qa-mesos-persistence:5000/gem-molo
+docker push qa-mesos-persistence.za.prk-host.net:5000/gem-molo
