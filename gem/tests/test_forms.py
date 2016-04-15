6f4ffaa6ac7a8627a129e78bf124ad9af869d3af
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-04-15 19:17:34 +0200

    added tests

---
+++ gem/tests/test_forms.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from gem.forms import RegistrationForm
+
+
+class RegisterTestCase(TestCase):
+
+    def test_register_username_correct(self):
+        form_data = {
+            'username': 'Jeyabal@-1',
+            'password': '1234',
+            'gender': 'f',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertEqual(form.is_valid(), True)
+
+    def test_register_username_incorrect(self):
+        form_data = {
+            'username': 'Jeyabal#',
+            'password': '1234',
+            'gender': 'f',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
+
+    def test_register_password_incorrect(self):
+        form_data = {
+            'username': 'Jeyabal#',
+            'password': '12345',
+            'gender': 'f',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
+
+    def test_username_exists(self):
+        User.objects.create_user(
+            'testing', 'testing@example.com', 'testing')
+        form_data = {
+            'username': 'testing',
+            'password': '12345',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        [validation_error] = form.errors.as_data()['username']
+        self.assertEqual(
+            'Username already exists.', validation_error.message)
+
+    def test_register_gender_required(self):
+        form_data = {
+            'username': 'Jeyabal@-1',
+            'password': '1234',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
---
+++ gem/tests/test_views.py
@@ -0,0 +1,25 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+
+from gem.forms import RegistrationForm
+
+
+class RegistrationViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_register_view(self):
+        response = self.client.get(reverse('user_register'))
+        self.assertTrue(isinstance(response.context['form'], RegistrationForm))
+
+    def test_register_view_invalid_form(self):
+        # NOTE: empty form submission
+        response = self.client.post(reverse('user_register'), {
+        })
+        self.assertFormError(
+            response, 'form', 'username', ['This field is required.'])
+        self.assertFormError(
+            response, 'form', 'password', ['This field is required.'])
+        self.assertFormError(
+            response, 'form', 'gender', ['This field is required.'])
