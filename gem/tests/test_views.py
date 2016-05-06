339babcaccdbd780b72b08ae4a52e54bad5a996a
Richard Landers <infolog>
2016-05-06 14:23:18 +0200

    added tests for comment filters

--- gem/tests/test_views.py
+++ gem/tests/test_views.py
@@ -5,6 +5,8 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.models import Site
+from gem.models import SiteSettings
+from molo.commenting import MoloCommentForm
 
 from molo.core.tests.base import MoloTestCaseMixin
 from molo.commenting.models import MoloComment
@@ -42,6 +44,13 @@ class CommentingTestCase(TestCase, MoloTestCaseMixin):
             password='tester')
         self.mk_main()
 
+        self.yourmind = self.mk_section(
+            self.main, title='Your mind')
+        self.article = self.mk_article(self.yourmind,
+                                       title='article 1',
+                                       subtitle='article 1 subtitle',
+                                       slug='article-1')
+
     def create_comment(self, article, comment, parent=None):
         return MoloComment.objects.create(
             content_type=ContentType.objects.get_for_model(article),
@@ -53,25 +62,61 @@ class CommentingTestCase(TestCase, MoloTestCaseMixin):
             parent=parent,
             submit_date=datetime.now())
 
-    def test_comment_shows_user_display_name(self):
-        self.yourmind = self.mk_section(
-            self.main, title='Your mind')
-        article = self.mk_article(self.yourmind, title='article 1',
-                                  subtitle='article 1 subtitle',
-                                  slug='article-1')
+    def getData(self):
+        return {
+            'name': self.user.username,
+            'email': self.user.email
+        }
 
+    def test_comment_shows_user_display_name(self):
         # check when user doesn't have an alias
-        self.create_comment(article, 'test comment1 text')
+        self.create_comment(self.article, 'test comment1 text')
         response = self.client.get('/your-mind/article-1/')
         self.assertContains(response, "Anonymous")
 
         # check when user have an alias
         self.user.profile.alias = 'this is my alias'
         self.user.profile.save()
-        self.create_comment(article, 'test comment2 text')
+        self.create_comment(self.article, 'test comment2 text')
         response = self.client.get('/your-mind/article-1/')
         self.assertContains(response, "this is my alias")
         self.assertNotContains(response, "tester")
+
+    def getValidData(self, obj):
+        form = MoloCommentForm(obj)
+        form_data = self.getData()
+        form_data.update(form.initial)
+        return form_data
+
+    def test_comment_filters(self):
+        site = Site.objects.get(id=1)
+        site.name = 'GEM'
+        site.save()
+        SiteSettings.objects.create(site_id=site.id,
+                                    banned_keywords_and_patterns='naughty')
+
+        form_data = self.getValidData(self.article)
+
+        # check if user has typed in a number
+        comment_form = MoloCommentForm(
+            self.article, data=dict(form_data, comment="0821111111")
+        )
+
+        self.assertFalse(comment_form.is_valid())
+
+        # check if user has typed in an email address
+        comment_form = MoloCommentForm(
+            self.article, data=dict(form_data, comment="test@test.com")
+        )
+
+        self.assertFalse(comment_form.is_valid())
+
+        # check if user has used a banned keyword
+        comment_form = MoloCommentForm(
+            self.article, data=dict(form_data, comment="naughty")
+        )
+
+        self.assertFalse(comment_form.is_valid())
 
 
 class GemFeedViewsTest(TestCase, MoloTestCaseMixin):
