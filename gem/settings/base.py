f5b5ffbe54cab80db7eca340f6cfa346d67ec886
Saeed Marzban <Saeed.mb8@gmail.com>
2016-05-06 18:20:10 +0200

    added RW locale to settings file

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 LANGUAGES = global_settings.LANGUAGES + (
     ('tl', _('Tagalog')),
-    ('kin', _('Kinyarwanda')),
+    ('rw', _('Kinyarwanda')),
 )
 LOCALE_PATHS = (
     join(PROJECT_ROOT, "locale"),
