ea723914efeeccc9be0da3ec3be61fa28ee12cc3
Saeed Marzban <Saeed.mb8@gmail.com>
2016-06-07 05:58:02 +0200

    make sure LANGUAGE_CODE is env variable

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -181,7 +181,7 @@ CELERYBEAT_SCHEDULE = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-gb'
+LANGUAGE_CODE = environ.get('LANGUAGE_CODE', 'en-gb')
 TIME_ZONE = 'Africa/Johannesburg'
 USE_I18N = True
 USE_L10N = True
