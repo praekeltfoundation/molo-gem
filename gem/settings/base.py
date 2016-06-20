ac2e6c3cf2e68835843a32aed7b07bae5394b5b8
Saeed Marzban <saeed@praekeltfoundation.org>
2016-06-20 17:44:12 +0200

    Added wagtailmedia to installed app

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     'wagtail.wagtailredirects',
     'wagtail.wagtailforms',
     'wagtailmodeladmin',
+    'wagtailmedia',
     'wagtail.contrib.settings',
     'wagtailsurveys',
 
