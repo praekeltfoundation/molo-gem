8adc92f6f36ce91eaff550f751270f54737c5206
Richard Landers <infolog>
2016-06-28 11:18:38 +0200

    added GemReportComment Model and Admin

--- gem/admin.py
+++ gem/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from gem.models import GemUserProfile
+from gem.models import GemUserProfile, GemReportComment
+from molo.commenting.admin import MoloCommentAdmin
+from molo.commenting.models import MoloComment
 
 
 class GemUserProfileInlineModelAdmin(admin.StackedInline):
     model = GemUserProfile
+    can_delete = False
+
+
+class GemReportCommentModelAdmin(admin.StackedInline):
+    model = GemReportComment
     can_delete = False
 
 
@@ -17,5 +24,16 @@ class GemUserAdmin(UserAdmin):
         return obj.gem_profile.get_gender_display()
 
 
+class GemReportCommentAdmin(MoloCommentAdmin):
+    inlines = (GemReportCommentModelAdmin,)
+    list_display = MoloCommentAdmin.list_display + ('reported_for', )
+
+    def reported_for(self, obj):
+        return obj.gem_comment.get_reported_reason_display()
+
+
 admin.site.unregister(User)
 admin.site.register(User, GemUserAdmin)
+
+admin.site.unregister(MoloComment)
+admin.site.register(MoloComment, GemReportCommentAdmin)
