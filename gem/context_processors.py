d23bdbbf604d7820c5096e14d19793b77bc07338
Codie Roelf <codie@praekelt.com>
2016-07-22 12:16:10 +0200

    removing context processor and adding analytics settings

--- gem/context_processors.py
+++ gem/context_processors.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from molo.profiles.forms import RegistrationForm
 from molo.profiles.forms import EditProfileForm, ProfilePasswordChangeForm
-from molo.core.models import SiteSettings
 
 
 def default_forms(request):
@@ -9,12 +8,4 @@ def default_forms(request):
         'registration_form': RegistrationForm(),
         'edit_profile_form': EditProfileForm(),
         'password_change_form': ProfilePasswordChangeForm()
-    }
-
-
-def add_tag_manager_account(request):
-    site_settings = SiteSettings.for_site(request.site)
-    return {
-        'GOOGLE_TAG_MANAGER_ACCOUNT': (site_settings.ga_tag_manager or
-                                       settings.GOOGLE_TAG_MANAGER_ACCOUNT)
     }
