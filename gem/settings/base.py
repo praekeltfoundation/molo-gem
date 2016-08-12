01941794b8008513f6927765e0476e196f381e6a
Milton Madanda <madandat@gmail.com>
2016-08-12 10:20:29 +0200

    specify FBS_UIP

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -300,6 +300,7 @@ _("Name of your primary school")
 SITE_STATIC_PREFIX = environ.get('SITE_STATIC_PREFIX', '').lower()
 
 GOOGLE_TAG_MANAGER_ACCOUNT = environ.get('GOOGLE_TAG_MANAGER_ACCOUNT')
+CUSTOM_UIP_HEADER = 'HTTP_X_IORG_FBS_UIP'
 
 # Password reset - security questions
 SECURITY_QUESTION_1 = environ.get(
