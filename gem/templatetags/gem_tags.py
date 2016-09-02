9fbcf7ec9bfda05d601f97cc150946fc76ca7090
Saeed Marzban <Saeed.mb8@gmail.com>
2016-09-02 14:34:31 +0200

    Add fieldtype template tag

--- gem/templatetags/gem_tags.py
+++ gem/templatetags/gem_tags.py
@@ -7,3 +7,8 @@ register = Library()
 @register.simple_tag()
 def get_site_static_prefix():
     return settings.SITE_STATIC_PREFIX
+
+
+@register.filter('fieldtype')
+def fieldtype(field):
+    return field.field.widget.__class__.__name__
