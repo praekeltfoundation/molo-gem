0f436c654abe67a1d771a485a6dd1d3ee0ba83d1
Mitso <mqalaba@gmail.com>
2017-04-20 17:28:54 +0200

    update static file dir

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -277,6 +277,10 @@ STATIC_ROOT = join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 COMPRESS_ENABLED = True
 
+STATICFILES_DIRS = [
+    'molo.core/static/'
+]
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
