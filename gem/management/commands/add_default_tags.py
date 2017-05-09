7a3613ff7b26068d41128db7186a8813558840e3
Saeed Marzban <Saeed.mb8@gmail.com>
2017-05-09 14:59:51 +0200

    Add error messages

--- gem/management/commands/add_default_tags.py
+++ gem/management/commands/add_default_tags.py
@@ -24,25 +24,35 @@ class Command(BaseCommand):
                 main.get_site()).languages.filter(
                 is_active=True, is_main_language=False).all()
             if main_lang:
-                add_tags(main_lang, child_languages, tag_index, tags)
+                add_tags(self, main_lang, child_languages, tag_index, tags)
+            else:
+                self.stdout.write(self.style.NOTICE(
+                    'Main language does not exist in "%s"' % main))
 
 
-def add_tags(main_lang, child_languages, tag_index, tags):
+def add_tags(self, main_lang, child_languages, tag_index, tags):
     for tag in tags:
         if tags.get(tag).get(main_lang.locale):
             main_tag = create_tag(
                 tags.get(tag).get(main_lang.locale), tag_index)
+        else:
+            self.stdout.write(self.style.NOTICE(
+                'Tag %s does not exist for %s in the CSV' % (tag, main_lang)))
 
             for child_lang in child_languages:
                 if tags.get(tag).get(child_lang.locale):
                     create_tag_translation(
                         main_tag, child_lang, tags.get(tag).get(
                             child_lang.locale), tag_index)
+                else:
+                    self.stdout.write(self.style.NOTICE(
+                        'Tag %s does not exist for %s in the CSV'
+                        % (tag, child_lang)))
 
 
 def create_tag(title, tag_index):
     if Tag.objects.filter(title=title).child_of(tag_index).exists():
-        return Tag.objects.filter(title=title).first()
+        return Tag.objects.filter(title=title).child_of(tag_index).first()
     else:
         tag = Tag(title=title)
         tag_index.add_child(instance=tag)
