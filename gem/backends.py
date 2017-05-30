9b2bb5b41a9ab42a82d5145227988faf0d49d82b
Milton Madanda <madandat@gmail.com>
2017-05-30 10:29:54 +0200

    add null check in backend for CAS

--- gem/backends.py
+++ gem/backends.py
@@ -9,13 +9,15 @@ class GemModelBackend(MoloModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None:
             username = kwargs.get(UserModel.USERNAME_FIELD)
-        try:
-            user = UserModel.objects.get(
-                gem_profile__migrated_username=username,
-                profile__site=request.site)
-            username = user.username
-        except UserModel.DoesNotExist:
-            UserModel().set_password(password)
+
+        if username is not None:
+            try:
+                user = UserModel.objects.get(
+                    gem_profile__migrated_username=username,
+                    profile__site=request.site)
+                username = user.username
+            except UserModel.DoesNotExist:
+                UserModel().set_password(password)
 
         return super(GemModelBackend, self).authenticate(
             request=request, username=username, password=password, **kwargs)
