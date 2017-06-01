73f43bfc8bf25a64938dec5007777b22a67698c7
Saeed Marzban <Saeed.mb8@gmail.com>
2017-06-01 18:10:15 +0200

    add tests for all the languages

--- gem/tests/test_management_commands.py
+++ gem/tests/test_management_commands.py
@@ -83,5 +83,146 @@ class GemManagementCommandsTest(TestCase, MoloTestCaseMixin):
         self.assertEqual(reaction_question.count(), 16)
         choices = ReactionQuestionChoice.objects.child_of(
             reaction_question.first())
-
         self.assertEqual(choices.count(), 12)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='bn',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 20)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 15)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='ur',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 24)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 18)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='sw',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 28)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 21)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='th',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 32)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 24)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='pt',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 36)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 27)
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='ru',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 40)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 30)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='km',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 44)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 33)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='my',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 48)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 36)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='id',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 52)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 39)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='ha',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 56)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 42)
+
+        SiteLanguageRelation.objects.create(
+            language_setting=self.language_setting,
+            locale='tl',
+            is_active=True)
+        out = StringIO()
+        call_command('add_reaction_questions_and_choices', stdout=out)
+        self.assertIn('', out.getvalue())
+        reaction_question = ReactionQuestion.objects.all()
+        self.assertEqual(reaction_question.count(), 60)
+        choices = ReactionQuestionChoice.objects.child_of(
+            reaction_question.first())
+        self.assertEqual(choices.count(), 45)
