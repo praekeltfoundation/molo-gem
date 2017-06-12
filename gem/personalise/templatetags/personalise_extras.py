46cec51fa3b81da21662da5d36ccaf1f409caaea
Tomasz Knapik <tomasz.knapik@torchbox.com>
2017-06-12 14:37:05 +0100

    Fix error when displaying other types of surveys

--- gem/personalise/templatetags/personalise_extras.py
+++ gem/personalise/templatetags/personalise_extras.py
@@ -9,12 +9,11 @@ def filter_surveys_by_segments(surveys, request):
     """Filter out surveys not in user's segments."""
     user_segments = get_segment_adapter(request).get_segments()
     user_segments_ids = [s.id for s in user_segments]
-    filetered_surveys = []
+    filtered_surveys = []
 
     for survey in surveys:
-        if not survey.segment_id:
-            filetered_surveys.append(survey)
-        elif survey.segment_id in user_segments_ids:
-            filetered_surveys.append(survey)
+        if not hasattr(survey, 'segment_id') or not survey.segment_id \
+                or survey.segment_id in user_segments_ids:
+            filtered_surveys.append(survey)
 
-    return filetered_surveys
+    return filtered_surveys
