0f76a05f44503d60d711c03202b771ebfa5eb67b
Jamie Hewland <jhewland@gmail.com>
2017-06-20 12:23:33 +0200

    Don't delete that line

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -280,6 +280,7 @@ STATIC_ROOT = join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 COMPRESS_ENABLED = True
 
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
