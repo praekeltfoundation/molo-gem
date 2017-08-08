fc84d1797cd6322126c43f2931e29817a8309d6e
nathanbegbie <nathanbegbie@gmail.com>
2017-08-08 18:16:39 +0200

    Copy over Molo Core GA middleware

--- gem/middleware.py
+++ gem/middleware.py
@@ -1,3 +1,10 @@
+from bs4 import BeautifulSoup
+from django.conf import settings
+from google_analytics.utils import build_ga_params, set_cookie
+from google_analytics.tasks import send_ga_tracking
+
+from molo.core.models import SiteSettings
+
 
 class ForceDefaultLanguageMiddleware(object):
     """
@@ -21,3 +28,47 @@ class LogHeaderInformationMiddleware(object):
     def process_request(self, request):
         print '---------- Header Dump -------------'
         print request.META.items()
+
+
+class MoloGoogleAnalyticsMiddleware(object):
+    """Uses GA IDs stored in Wagtail to track pageviews using celery"""
+    def submit_tracking(self, account, request, response):
+        try:
+            title = BeautifulSoup(
+                response.content, "html.parser"
+            ).html.head.title.text.encode('utf-8')
+        except:
+            title = None
+
+        path = request.get_full_path()
+        referer = request.META.get('HTTP_REFERER', '')
+        params = build_ga_params(
+            request, account, path=path, referer=referer, title=title)
+        response = set_cookie(params, response)
+        send_ga_tracking.delay(params)
+        return response
+
+    def process_response(self, request, response):
+        if hasattr(settings, 'GOOGLE_ANALYTICS_IGNORE_PATH'):
+            exclude = [p for p in settings.GOOGLE_ANALYTICS_IGNORE_PATH
+                       if request.path.startswith(p)]
+            if any(exclude):
+                return response
+
+        # Only track 200 and 302 responses for request.site
+        if not (response.status_code == 200 or response.status_code == 302):
+            return response
+
+        site_settings = SiteSettings.for_site(request.site)
+        local_ga_account = site_settings.local_ga_tracking_code or \
+            settings.GOOGLE_ANALYTICS.get('google_analytics_id')
+
+        if local_ga_account:
+            response = self.submit_tracking(
+                local_ga_account, request, response)
+
+        if site_settings.global_ga_tracking_code:
+            response = self.submit_tracking(
+                site_settings.global_ga_tracking_code, request, response)
+
+        return response
