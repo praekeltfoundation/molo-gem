5b5b6e627ccc2d0709cb3e9cf63be302ee914f3c
Johan Bouwer <projectmushroom@me.com>
2017-08-23 18:02:52 +0200

    temp csrf fix

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -112,7 +112,7 @@ MIDDLEWARE_CLASSES = [
     'molo.core.middleware.ForceDefaultLanguageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
