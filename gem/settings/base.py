f4790d9aaba56a32927bfd8455c696f05bfa2f0d
Johan Bouwer <projectmushroom@me.com>
2017-08-31 14:28:39 +0200

    update icons

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -131,7 +131,7 @@ MIDDLEWARE_CLASSES = [
 
 # We have multiple layouts: use `base`, `malawi` or `springster`
 # to switch between them.
-SITE_LAYOUT_BASE = environ.get('SITE_LAYOUT_BASE', 'base')
+SITE_LAYOUT_BASE = environ.get('SITE_LAYOUT_BASE', 'springster')
 SITE_LAYOUT_2 = environ.get('SITE_LAYOUT_2', '')
 
 TEMPLATES = [
