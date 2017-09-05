29920a8bcbef666871d8d55921b40d26e9daecd6
Johan Bouwer <projectmushroom@me.com>
2017-09-05 16:49:58 +0200

    update templates + add is_via_freebasics

--- gem/context_processors.py
+++ gem/context_processors.py
@@ -8,3 +8,12 @@ def default_forms(request):
         'edit_profile_form': EditProfileForm(),
         'password_change_form': ProfilePasswordChangeForm()
     }
+
+
+def detect_freebasics(request):
+    return {
+        'is_via_freebasics':
+            'internet.org' in request.META['HTTP_VIA'] or
+            'InternetOrgApp' in request.META['HTTP_USER_AGENT'] or
+            'HTTP_X_IORG_FBS' in request.META,
+    }
