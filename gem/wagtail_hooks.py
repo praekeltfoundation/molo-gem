10009be3973697ea6f58ff58f74cd5cc6c29185f
nathanbegbie <nathanbegbie@gmail.com>
2017-11-10 17:13:22 +0200

    desperate times

--- gem/wagtail_hooks.py
+++ gem/wagtail_hooks.py
@@ -3,3 +3,16 @@ from wagtail.contrib.modeladmin.options import modeladmin_register
 
 
 modeladmin_register(GemFrontendUsersModelAdmin)
+
+
+@hooks.register('insert_global_admin_js')
+def global_admin_js():
+    js_files = [
+        'js/modeladmin/index.js',
+    ]
+
+    js_includes = format_html_join(
+        '\n', '<script src="{0}{1}"></script>',
+        ((settings.STATIC_URL, filename) for filename in js_files)
+    )
+    return js_includes
