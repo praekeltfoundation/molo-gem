8c5814dd242928cbb69e8799eed90af86654c407
Alex Muller <alex@praekelt.org>
2018-01-08 10:11:16 +0000

    Remove VERSION file from root directory
    This hasn't been touched in a while

--- setup.py
+++ setup.py
@@ -9,11 +9,8 @@ with open(os.path.join(here, 'README.rst')) as f:
 with open(os.path.join(here, 'requirements.txt')) as f:
     requires = filter(None, f.readlines())
 
-with open(os.path.join(here, 'VERSION')) as f:
-    version = f.read().strip()
-
 setup(name='gem',
-      version=version,
+      version='0.0.1',
       description='gem',
       long_description=README,
       classifiers=[
