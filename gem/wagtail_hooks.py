d0c173593a1c2f1999224bce42dddd71aa8104d8
Codie Roelf <codie@praekelt.com>
2018-01-08 18:58:17 +0000

    remove duplicate comments

--- gem/wagtail_hooks.py
+++ gem/wagtail_hooks.py
@@ -1,5 +1,12 @@
 from gem.admin import GemFrontendUsersModelAdmin, GemCommentModelAdmin
 from wagtail.contrib.modeladmin.options import modeladmin_register
+from wagtail.wagtailcore import hooks
 
 modeladmin_register(GemFrontendUsersModelAdmin)
 modeladmin_register(GemCommentModelAdmin)
+
+
+@hooks.register('construct_main_menu')
+def hide_duplicate_comments_menu_item(request, menu_items):
+    menu_items[:] = [
+        item for item in menu_items if not item.name == 'comments']
