36ff85b34498545dd3f2a7f6b387fb5efbc88da1
Codie Roelf <codie@praekelt.com>
2018-01-29 15:42:10 +0000

    refactor tests

--- gem/tests/test_profiles.py
+++ gem/tests/test_profiles.py
@@ -4,23 +4,18 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 
-class GemRegistrationViewTest(TestCase, MoloTestCaseMixin):
+class TestProfileInformationDisplay(TestCase, MoloTestCaseMixin):
     def setUp(self):
         self.mk_main()
         self.client = Client()
 
-    def test_user_info_displaying_after_registration(self):
+    def test_gender_displays_correctly(self):
         self.user = User.objects.create_user(
             username='tester',
             email='tester@example.com',
             password='tester')
         self.client.login(username='tester', password='tester')
-        response = self.client.get(reverse('edit_my_profile'))
-        self.assertNotContains(response, 'useralias')
         self.user.profile.gender = 'f'
-        self.user.profile.alias = 'useralias'
         self.user.profile.save()
-        response = self.client.get(reverse('edit_my_profile'))
-        self.assertContains(response, 'useralias')
-        self.assertContains(
-            response, '<option value="f" selected>female</option>')
+        response = self.client.get(reverse('molo.profiles:view_my_profile'))
+        self.assertContains(response, '<span>female</span>')
