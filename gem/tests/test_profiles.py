614258179fba0e3c06227d583615f278b01448d5
Codie Roelf <codie@praekelt.com>
2018-02-07 16:20:19 +0000

    add profile settings to tests

--- gem/tests/test_profiles.py
+++ gem/tests/test_profiles.py
@@ -1,4 +1,5 @@
 from molo.core.tests.base import MoloTestCaseMixin
+from molo.profiles.models import UserProfilesSettings
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
@@ -8,6 +9,11 @@ class TestProfileInformationDisplay(TestCase, MoloTestCaseMixin):
     def setUp(self):
         self.mk_main()
         self.client = Client()
+        profile_settings = UserProfilesSettings.for_site(self.main.get_site())
+        profile_settings.activate_gender = True
+        profile_settings.capture_gender_on_reg = True
+        profile_settings.gender_required = True
+        profile_settings.save()
 
     def test_gender_displays_correctly(self):
         self.user = User.objects.create_user(
