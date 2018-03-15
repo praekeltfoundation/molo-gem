0b0648f5bf94ccdf6d21c646210eb5ba1e67ea9a
Saeed Marzban <Saeed.mb8@gmail.com>
2018-03-15 17:56:16 +0200

    Disable SSO

--- gem/settings/production.py
+++ gem/settings/production.py
@@ -61,17 +61,17 @@ GOOGLE_PLACES_API_SERVER_KEY = os.environ.get(
 )
 
 # Setup for CAS
-ENABLE_SSO = True
+ENABLE_SSO = False
 
-MIDDLEWARE_CLASSES += [  # noqa: F405
-    'molo.core.middleware.MoloCASMiddleware',
-    'molo.core.middleware.Custom403Middleware',
-]
+# MIDDLEWARE_CLASSES += [  # noqa: F405
+#     'molo.core.middleware.MoloCASMiddleware',
+#     'molo.core.middleware.Custom403Middleware',
+# ]
 
-CAS_SERVER_URL = ''
-CAS_ADMIN_PREFIX = '/admin/'
-LOGIN_URL = 'molo.profiles:auth_login'
-CAS_VERSION = '3'
+# CAS_SERVER_URL = ''
+# CAS_ADMIN_PREFIX = '/admin/'
+# LOGIN_URL = 'molo.profiles:auth_login'
+# CAS_VERSION = '3'
 
 
 COMPRESS_OFFLINE_CONTEXT = {  # noqa
