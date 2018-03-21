2bbf6e339bee32a04b027b57ffe78577be4485b2
Alex Muller <alex@praekelt.org>
2018-03-21 11:46:55 +0000

    Remove unneded middleware
    This is also in molo.core.

--- gem/middleware.py
+++ gem/middleware.py
@@ -6,23 +6,6 @@ from molo.core.middleware import MoloGoogleAnalyticsMiddleware
 from gem.models import GemSettings
 
 
-class ForceDefaultLanguageMiddleware(object):
-    """
-    Ignore Accept-Language HTTP headers
-
-    This will force the I18N machinery to always choose settings.LANGUAGE_CODE
-    as the default initial language, unless another one is set via sessions or
-    cookies
-
-    Should be installed *before* any middleware that
-    checks request.META['HTTP_ACCEPT_LANGUAGE'],
-    namely django.middleware.locale.LocaleMiddleware
-    """
-    def process_request(self, request):
-        if 'HTTP_ACCEPT_LANGUAGE' in request.META:
-            del request.META['HTTP_ACCEPT_LANGUAGE']
-
-
 class GemMoloGoogleAnalyticsMiddleware(MoloGoogleAnalyticsMiddleware):
     '''
     Submits GA tracking data to a local account or the additional account
