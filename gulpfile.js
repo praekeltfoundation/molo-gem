7c6d938c09dfbde4ecab9a3f78342ac5ecb3fe3b
Mitso <mqalaba@gmail.com>
2018-03-22 17:59:42 +0200

    update styles implementation

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,8 @@ var gulp              =   require('gulp'),
         'gem/styles/gem-springster/02_springster-rtl.s+(a|c)ss',
         'gem/styles/gem-springster/03_state.s+(a|c)ss',
         'gem/styles/gem-springster/04_state-320.s+(a|c)ss',
-        'gem/styles/gem-springster/05_no-script-state.s+(a|c)ss'
+        'gem/styles/gem-springster/05_no-script-state.s+(a|c)ss',
+        'gem/styles/gem-springster/@font-face-baton.s+(a|c)ss'
     ],
     sassDest = {
          prd: 'gem/static/css/prd',
