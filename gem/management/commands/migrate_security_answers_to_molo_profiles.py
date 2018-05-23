7b90ad1c635e8b88b193d19d2847e5bb2eaabed7
Alex Muller <alex@praekelt.org>
2018-05-23 10:06:18 +0100

    Remove management command to migrate security answers to molo.profiles
    This has been run in QA and production so it's dead code that we'll never
    need again. It's used in a migration though so we shouldn't remove the
    command entirely.

--- gem/management/commands/migrate_security_answers_to_molo_profiles.py
+++ gem/management/commands/migrate_security_answers_to_molo_profiles.py
@@ -1,26 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import logging
-
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 from django.core.management.base import BaseCommand
-
-from gem.tasks import migrate_security_answers_to_profiles
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for question_number in range(1, 3):
-            question_setting = 'SECURITY_QUESTION_{0}'.format(question_number)
-            question_text = getattr(settings, question_setting, None)
-
-            if question_text is None:
-                raise ImproperlyConfigured(
-                    'Security question {0} is unset'.format(question_setting))
-
-            logging.info('Migrating answers for {0}'.format(question_setting))
-
-            migrate_security_answers_to_profiles.delay(
-                question_text, question_number, question_setting)
+        # This management command was created to migrate security answers from
+        # a user's gem_profile to their molo profile. It's referenced in
+        # migration 0026 so we can't remove the whole command without modifying
+        # the file, but we it's in production so we never need to run it again.
+        pass
