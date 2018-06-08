4a64b5ac8656b9d97f08d10d0d814a3d0a899d3f
Codie Roelf <codie@praekelt.com>
2018-06-08 11:28:49 +0200

    remove gem tasks from celery settings

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -242,7 +242,7 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ALWAYS_EAGER = False
-CELERY_IMPORTS = ('gem.tasks', 'molo.core.tasks', 'google_analytics.tasks')
+CELERY_IMPORTS = ('molo.core.tasks', 'google_analytics.tasks')
 BROKER_URL = environ.get('BROKER_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = environ.get(
     'CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
