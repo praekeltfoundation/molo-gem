1f553b6c3861df56f9f2a5f8dd9c2d7b97c7b63e
Codie Roelf <codie@praekelt.com>
2018-06-08 11:35:59 +0200

    add molo commenting tasks

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -243,7 +243,8 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ALWAYS_EAGER = False
 CELERY_IMPORTS = (
-    'molo.core.tasks', 'google_analytics.tasks', 'molo.profiles.task')
+    'molo.core.tasks', 'google_analytics.tasks', 'molo.profiles.task',
+    'molo.commenting.tasks')
 BROKER_URL = environ.get('BROKER_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = environ.get(
     'CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
