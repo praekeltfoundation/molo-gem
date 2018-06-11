69c077d53485b8ea02966306ebf8c9c8bcca59c5
Saeed Marzban <Saeed.mb8@gmail.com>
2018-06-11 23:51:15 +0200

    rename the migration

--- gem/migrations/0031_add_view_yourwords_entry_to_groups.py
+++ gem/migrations/0031_add_view_yourwords_entry_to_groups.py
@@ -7,7 +7,7 @@ from django.db import migrations
 from django.core.management.sql import emit_post_migrate_signal
 
 
-def create_groups_and_permissions(apps, schema_editor):
+def add_view_yourwords_entry_to_groups(apps, schema_editor):
     db_alias = schema_editor.connection.alias
 
     emit_post_migrate_signal(2, False, db_alias)
@@ -15,12 +15,12 @@ def create_groups_and_permissions(apps, schema_editor):
     Group = apps.get_model('auth.Group')
     Permission = apps.get_model('auth.Permission')
 
-    view_yourwords_entries = Permission.objects.get(codename='can_view_entry')
+    view_yourwords_entry = Permission.objects.get(codename='can_view_entry')
     group_names = ('product_admin', 'data_admin', 'data_viewer',
                    'content_admin', 'content_admin')
     for group_name in group_names:
         group = Group.objects.get(name=group_name).first()
-        group.permissions.add(view_yourwords_entries)
+        group.permissions.add(view_yourwords_entry)
 
 
 class Migration(migrations.Migration):
@@ -31,5 +31,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_groups_and_permissions),
+        migrations.RunPython(add_view_yourwords_entry_to_groups),
     ]
