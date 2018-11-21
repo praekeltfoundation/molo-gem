99dfe2992c6e9d68683a497774664721d694e41e
Mitso <mqalaba@gmail.com>
2018-11-21 14:06:12 +0200

    update polls templates

--- gem/static/js/menu.js
+++ gem/static/js/menu.js
@@ -5,17 +5,24 @@
   };
 
   var menuToggle = function() {
-      var menuListLabel = document.getElementById('nav_slide');
-      var menuList = document.querySelector('.menu--header');
-      menuListLabel.addEventListener('click', function() {
-        var $label = event.target.previousElementSibling;
-        $label.classList.toggle('nav__active');
-        menuList.classList.toggle('nav__is-open');
-      });
+      var menuListLabel     = document.getElementById('burger_nav_label');
+      var menuListLabelOpen = document.querySelector('.is__open');
+      //Traverse the entire page to look for this specific class
+        menuListLabel.addEventListener('click', function(e) {
+          if (e.target) {
+            menuListLabel.classList.add('is__open');
+            //menuListLabel.classList.remove("is__open");
+          }
+        var $label = e.target;
+          console.log($label);
+      }, false);
+      menuListLabelOpen.addEventListener('click', function(e) {
+        if (e.target) {
+          menuListLabelOpen.classList.remove("is__open");
+        }
+      }, false);
   }
-
   domReady(function() {
     menuToggle();
   });
-
 })();
