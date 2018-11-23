7df80821af7ae3a86f3208bd988945322753ad34
Mitso <mqalaba@gmail.com>
2018-11-23 15:59:11 +0200

    cleanup optmization styles

---
+++ gem/static/js/modeladmin/index.js
@@ -0,0 +1,19 @@
+(function($) {
+    console.log("modeladmin/index.js");
+    $(function() {
+        var userCheckboxes = $('input:checkbox.user');
+        userCheckboxes.each( function(i, element) {
+            var $element = $(element);
+            var id = $element.attr('name');
+            var target = $('.field-id').filter( function () {
+                return $(this).text().startsWith(id + '\n');
+            });
+            var cloned = $element.clone();
+            target.prepend(cloned);
+            cloned.change(function () {
+                $element.prop('checked', cloned.is(':checked'));
+            });
+        });
+        userCheckboxes.hide();
+    });
+})(jQuery);
