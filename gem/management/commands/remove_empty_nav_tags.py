1a90aec82ee0ab73b55148e88cec7ed454c797fb
sewagodimo <edneciamat@gmail.com>
2018-11-29 13:51:55 +0200

    remove print and fix flake8 error

--- gem/management/commands/remove_empty_nav_tags.py
+++ gem/management/commands/remove_empty_nav_tags.py
@@ -12,13 +12,12 @@ class Command(BaseCommand):
         for article in ArticlePage.objects.all():
             for nav_tag in article.nav_tags.all():
                 if nav_tag.tag is None:
-                	nav_tag.delete()
+                    nav_tag.delete()
                     try:
                         if article.live:
                             article.save_revision().publish()
                         else:
                             article.save_revision()
-                        print("nothing raised")
                     except IntegrityError:
                             return ("IntegrityError: Only articles with sites "
                                     "can be saved")
