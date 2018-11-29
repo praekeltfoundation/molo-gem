e7e30797062e7a6208c689193e420a03dc6e76ac
sewagodimo <edneciamat@gmail.com>
2018-11-29 12:12:05 +0200

    check for errors and only save changed files

--- gem/management/commands/remove_empty_nav_tags.py
+++ gem/management/commands/remove_empty_nav_tags.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.core.management.base import BaseCommand
 from molo.core.models import ArticlePage
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
@@ -11,9 +12,12 @@ class Command(BaseCommand):
         for article in ArticlePage.objects.all():
             for nav_tag in article.nav_tags.all():
                 if nav_tag.tag is None:
-                    nav_tag.delete()
-
-            if article.live:
-                article.save_revision().publish()
-            else:
-                article.save_revision()
+                    try:
+                        nav_tag.delete()
+                        if article.live:
+                            article.save_revision().publish()
+                        else:
+                            article.save_revision()
+                    except IntegrityError:
+                            print("IntegrityError: Articles that belong sites "
+                                  "taht are not used cannot be saved")
