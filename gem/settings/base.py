41b92bd51b5700c5a293498c06b0475c2b5ce9e7
sewagodimo <edneciamat@gmail.com>
2019-01-17 13:58:48 +0200

    add django x_frame_option settings

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -403,6 +403,7 @@ if ES_HOST and ES_SELECTED_INDEX:
 WAGTAILIMAGES_FEATURE_DETECTION_ENABLED = False
 IMAGE_COMPRESSION_QUALITY = 85
 
+
 # This setting lets you override the maximum upload size for images (in bytes).
 # If omitted, Wagtail will fall back to using its 10MB default value.
 # http://docs.wagtail.io/en/v1.12.2/advanced_topics/settings.html?#maximum-upload-size-for-images
@@ -574,4 +575,6 @@ AWS_S3_FILE_OVERWRITE = False
 PERSONALISATION_SEGMENTS_ADAPTER = (
     'molo.surveys.adapters.PersistentSurveysSegmentsAdapter'
 )
+
+X_FRAME_OPTIONS = "allow-from https://tableau.ie.gehosting.org"
 CSP_FRAME_ANCESTORS = 'self' '*.example.com' 'https://tableau.ie.gehosting.org'
