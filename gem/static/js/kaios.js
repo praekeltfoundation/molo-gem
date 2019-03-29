b840486905e6c63cac8bd5f071e14efde7210716
Mitso Qalaba <mqalaba@gmail.com>
2019-03-29 09:24:11 +0200

    update js

--- gem/static/js/kaios.js
+++ gem/static/js/kaios.js
@@ -1,51 +1,48 @@
 "use strict";
 (function() {
-  var domReady = function(callback) {
+  function domReady(callback) {
       document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
   };
 
-  function handleKeydown(e) {
-    switch(e.key) {
-      case 'ArrowUp':
-        nav(-1);
-        break;
-      case 'ArrowDown':
-        nav(1);
-        break;
-      case 'ArrowRight':
-        nav(1);
-        break;
-      case 'ArrowLeft':
-        nav(-1);
-        break;
-    }
-  };
-
-  var kaiosPadNav = function(e) {
+  function kaiosPadNav (e) {
     var childElem = document.querySelectorAll('div, a, input, button, select, textarea,li');
+    function nav(num) {
+      var currentIndex = childElem[num];
+      var next = currentIndex + num;
+      var items = document.querySelectorAll('.items');
+      var targetElement = items[next];
+      targetElement.focus();
+      console.log("My value: ", targetElement);
+    }
     for (var i = 0, len = childElem.length; i < len; i++) {
       childElem[i].className += " items";
       childElem[i].setAttribute('tabindex', i);
-      childElem[i].onfocus = function() {
-        document.activeElement.addEventListener('keydown', handleKeydown);
-      };
+      nav(i);
     }
+
+    var handleKeydown = function(e) {
+      switch(e.key) {
+        case 'ArrowUp':
+          nav(-1);
+          break;
+        case 'ArrowDown':
+          nav(1);
+          break;
+        case 'ArrowRight':
+          nav(1);
+          break;
+        case 'ArrowLeft':
+          nav(-1);
+          break;
+      }
+    };
+    document.activeElement.addEventListener('keydown', handleKeydown);
   };
-  //STEP 6 Do not work and does not make sense - Already applied focus
-  /*function nav (e) {
-    var currentIndex = document.activeElement;
-    var next = currentIndex + e;
-    var items = document.querySelectorAll('.items');
-    var targetElement = items[next];
-    targetElement.focus();
-  };*/
-
-
 
   var softkeyCallback = {
-    left: function() { console.log('You click on SoftLeft') },
-    center: function() { console.log('You click on Enter') },
-    right: function() { console.log('You click on SoftRight') }
+    left: function() { /*console.log('You click on SoftLeft')*/ },
+    center: function() { /*console.log('You click on Enter')*/ },
+    right: function() { /*console.log('You click on SoftRight')*/ }
   };
   function handleKeyDownEvent(evt) {
     switch (evt.key) {
@@ -65,10 +62,9 @@
         break;
     }
   };
-  document.addEventListener('keydown', handleKeyDownEvent);
 
   domReady(function() {
     kaiosPadNav();
-
+    document.addEventListener('keydown', handleKeyDownEvent);
   });
 })();
