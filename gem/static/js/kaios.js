a02b8603be8378cb895487109df8b04afdc89ac2
Mitso Qalaba <mqalaba@gmail.com>
2019-04-05 12:25:21 +0200

    update scripts to work

--- gem/static/js/kaios.js
+++ gem/static/js/kaios.js
@@ -3,42 +3,50 @@
   function domReady(callback) {
       document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
   };
-
   function kaiosPadNav (e) {
     var childElems = document.querySelectorAll('div, a, input, button, select, textarea,li');
-    var currentIndex, bigScopeChildElems;
-    for (var i = 0, len = childElems.length; i < len; i++ ) {
-      childElems[i].className += " items";
-      childElems[i].setAttribute('tabindex', i);
-      if (childElems[i].getAttribute('tabindex') !== -1 ) {
-        currentIndex = i;
+    var currentIndex = 0, lastTabIndex, lastTabIndexElems, values = [];
+    var genericTabs = function (a) {
+      for(i = 0; i < a.length; i++ ){
+        a[i].className += " items";
+        a[i].setAttribute('tabindex', i);
+        values.push(a[i]);
       }
+      return values;
     }
-    bigScopeChildElems = childElems[currentIndex];
-    function nav(event) {
-      var next = (parseInt(bigScopeChildElems.getAttribute('tabindex'), 10) + event);
-      var items = document.querySelectorAll('.items');
+    genericTabs(childElems).forEach(function(item, index, array) {
+      lastTabIndex = index;
+      lastTabIndexElems = item;
+      values = array;
+      return lastTabIndex, lastTabIndexElems, values;
+    });
+
+    function nav(ev) {
+      var next = currentIndex + ev;
+      var items = document.querySelectorAll(".items");
+      currentIndex = next;
       var targetElement = items[next];
-        targetElement.focus();
-        console.log(items);
-    };
+      console.log(targetElement);
+      targetElement.focus();
+    }
+
     var handleKeydown = function(e) {
       switch(e.key) {
         case 'ArrowUp':
           nav(-1);
-          console.log('You click on ArrowUp', e.which || e.keyCode || 0);
+          console.log('You click on ArrowUp', e.which || e.keyCode);
           break;
         case 'ArrowDown':
           nav(1);
-          console.log('You click on ArrowDown', e.which || e.keyCode || 0)
+          console.log('You click on ArrowDown', e.which || e.keyCode)
           break;
         case 'ArrowRight':
           nav(1);
-          console.log('You click on ArrowRight', e.which || e.keyCode || 0)
+          console.log('You click on ArrowRight', e.which || e.keyCode)
           break;
         case 'ArrowLeft':
           nav(-1);
-          console.log('You click on ArrowLeft', e.which || e.keyCode || 0)
+          console.log('You click on ArrowLeft', e.which || e.keyCode)
           break;
       }
     };
@@ -49,7 +57,10 @@
 
   var softkeyCallback = {
     left: function() { console.log('You click on SoftLeft') },
-    center: function() { console.log('You click on Enter') },
+    center: function() {
+      console.log('You click on Enter')
+
+    },
     right: function() { console.log('You click on SoftRight') }
   };
   function handleKeyDownEvent(evt) {
