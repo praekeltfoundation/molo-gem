9c0725722ed5806f6fc6abb4c0c6f7f515facf62
Shanmukha ram kumar M <shanmukha@ivoryts.com>
2019-05-16 19:20:25 +0530

    Video Model Code integration
     Changes to be committed:
    	modified:   gem/models.py

--- gem/models.py
+++ gem/models.py
@@ -6,6 +6,7 @@ from molo.core.models import BannerPage, BannerIndexPage
 
 from wagtail.core.models import Site
 from wagtail.contrib.settings.models import BaseSetting
+
 from wagtail.contrib.settings.registry import register_setting
 from wagtail.admin.edit_handlers import (
     FieldPanel,
@@ -140,3 +141,35 @@ class GemCommentReport(models.Model):
 
     reported_reason = models.CharField(
         max_length=128, blank=False)
+
+
+# class VideosIndexPage(Page):
+#     intro = RichTextField(blank=True)
+#     content_panels = Page.content_panels + [
+#         FieldPanel('intro', classname="full")
+#     ]
+#     def get_context(self, request):
+#         context = super().get_context(request)
+#         # Add extra variables and return the updated context
+#         context['videos_entries'] = VideosPage.objects.live()
+#         context['latest_video'] = VideosPage.objects.latest('id')
+#         return context
+
+class VideosPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    videourl = models.CharField(max_length=250,null=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+        index.SearchField('videourl'),
+        ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+        FieldPanel('videourl'),
+    ]
