d48ed327cdb15d27b76cf2327a7867dbaa22cfe8
Saeed Marzban <saeed@praekelt.org>
2019-07-18 14:36:42 +0200

    use status_string

--- gem/management/commands/change_content_language.py
+++ gem/management/commands/change_content_language.py
@@ -1,6 +1,6 @@
 from molo.core import models
 from django.core.management.base import BaseCommand
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseCommand):
@@ -11,30 +11,29 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         old_lang_pk = options.get('old_lang_pk', None)
         new_lang_pk = options.get('new_lang_pk', None)
-        if not old_lang_pk or not new_lang_pk or old_lang_pk == new_lang_pk:
+        if not old_lang_pk or not new_lang_pk:
             self.stdout.write(self.style.WARNING(
-                "Invalid language primary keys were entered" +
-                str(old_lang_pk) + " and " + str(new_lang_pk)))
+                "Please enter the old language pk followed by the new language pk"))
+        
+        elif old_lang_pk == new_lang_pk:
+            self.stdout.write(self.style.WARNING(
+                "The old language pk and new language pk are the same."))
+
         else:
             try:
                 old_lang = models.SiteLanguage.objects.get(pk=old_lang_pk)
                 new_lang = models.SiteLanguage.objects.get(pk=new_lang_pk)
-                if not old_lang or not new_lang:
-                    self.stdout.write(self.style.WARNING(
-                        "Invalid language primary keys were entered" +
-                        str(old_lang_pk) + " and " + str(new_lang_pk)))
-                else:
-                    pages = models.MoloPage.objects.all()
-                    for page in pages:
-                        if hasattr(page.specific, 'language'):
-                            if page.specific.language == old_lang:
-                                page.specific.language = new_lang
-                                if not page.specific.has_unpublished_changes:
-                                    page.specific.save_revision().publish()
-                                else:
-                                    page.specific.save_revision().publish()
-                                    page.specific.unpublish()
-            except (ObjectDoesNotExist, MultipleObjectsReturned):
+                pages = models.MoloPage.objects.all()
+                for page in pages:
+                    if hasattr(page.specific, 'language'):
+                        if page.specific.language == old_lang:
+                            page.specific.language = new_lang
+                            if page.specific.status_string == 'live':
+                                page.specific.save_revision().publish()
+                            else:
+                                page.specific.save_revision().publish()
+                                page.specific.unpublish()
+            except (ObjectDoesNotExist):
                 self.stdout.write(self.style.WARNING(
-                    "Invalid language primary keys were entered" +
-                    str(old_lang_pk) + " and " + str(new_lang_pk)))
+                    "One or more of the languages with PKs" +
+                    str(old_lang_pk) + " and " + str(new_lang_pk) + " does not exist."))
