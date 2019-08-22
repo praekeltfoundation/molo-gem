597476dbb9fae21307750219fda6c1ed0f8e7e90
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-08-22 11:13:35 +0200

    rm depricating assertEquals in tests

--- gem/middleware.py
+++ gem/middleware.py
@@ -211,7 +211,7 @@ class GemMoloGoogleAnalyticsMiddleware(MoloGoogleAnalyticsMiddleware):
 
 class ChhaaJaaLoginMiddleware(object):
     def process_request(self, request):
-        if request.user.is_authenticated() is False \
+        if request.user.is_authenticated is False \
                 and settings.SITE_LAYOUT_BASE == 'chhaajaa' \
                 and (
                     'login' not in request.path and
