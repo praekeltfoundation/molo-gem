8015ff61a8858105728ae0b9cc9fec28c2c9adfb
Mitso Qalaba <mqalaba@gmail.com>
2019-09-12 16:19:17 +0200

    commit updates

---
+++ gem/static/js/Components/molo.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class MoloComp extends React.Component {
+  render() {
+    return (
+      <h3 className="heading--subheading">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+      </h3>
+    );
+  }
+}
+
+export {MoloComp};
