d03938ad3d75cfaf228e12c6b3fd2d85aeb35f9a
Mitso Qalaba <mqalaba@gmail.com>
2019-09-12 11:31:08 +0200

    commit updates

---
+++ config/webpack.prod.config.js
@@ -0,0 +1,22 @@
+const path = require("path"),
+      webpack = require('webpack'),
+      BundleTracker = require('webpack-bundle-tracker'),
+      config = require('./webpack.base.config.js');
+
+  config.output.path = require('path').resolve('./gem/static/js/dest')
+
+  config.plugins = config.plugins.concat([
+    new BundleTracker(
+      {
+        filename: './webpack-stats-prod.json'
+      }
+    ),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ])
+
+module.exports = config
