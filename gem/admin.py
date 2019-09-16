07e05e8e2a5e802d148f69253fb712e3d2a728fa
Mitso Qalaba <mqalaba@gmail.com>
2019-09-16 15:04:45 +0200

    flake 8 updates

--- gem/admin.py
+++ gem/admin.py
@@ -3,7 +3,10 @@ from collections import Counter
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-from gem.models import GemCommentReport, OIDCSettings, ThaQuestion, QuestionnaireChoice
+from gem.models import (
+    GemCommentReport, OIDCSettings,
+    ThaQuestion, QuestionnaireChoice
+)
 from gem.rules import ProfileDataRule, CommentCountRule
 
 from molo.commenting.admin import MoloCommentAdmin, MoloCommentsModelAdmin
@@ -80,17 +83,18 @@ class CommentCountRuleAdminInline(admin.TabularInline):
     """
     model = CommentCountRule
 
+
 class QuestionnaireChoiceAdminInline(admin.TabularInline):
     model = QuestionnaireChoice
+
 
 class ThaQuestionAdmin(admin.ModelAdmin):
     list_display = ("questionnaire_text", "pub_date")
     inlines = [QuestionnaireChoiceAdminInline]
 
-admin.site.unregister(User)
 
+admin.site.unregister(User)
 admin.site.unregister(MoloComment)
 admin.site.register(OIDCSettings, OIDCSettingsAdmin)
 admin.site.register(MoloComment, GemCommentReportAdmin)
-
 admin.site.register(ThaQuestion, ThaQuestionAdmin)
