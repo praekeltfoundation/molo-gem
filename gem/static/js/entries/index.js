7d6a16fd241123e73d67bce8d6f8d4bd1f88075b
Mitso Qalaba <mqalaba@gmail.com>
2019-09-16 16:57:34 +0200

    good ui template structure

--- gem/static/js/entries/index.js
+++ gem/static/js/entries/index.js
@@ -5,13 +5,13 @@ import ReactDOM from "react-dom"
 class Language extends React.Component {
   render() {
     var list = window.props;
-    return <div className="questionare">{list.map(item => <TestChild key={item.pk} question={item.question_text}/> )}</div>;
+    return <ul className="questionare">{list.map(item => <LanguageChild key={item.pk} question={item.questionnaire_text}/> )}</ul>;
   }
 }
 
 class LanguageChild extends React.Component {
   render() {
-    return <li><b>{this.props.question}</b></li>;
+    return <li className="questionare__item">{this.props.question}</li>;
   }
 }
 
