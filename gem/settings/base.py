dc20bc88755c174236119da9db28f9b31d1b1bc1
Mitso Qalaba <mqalaba@gmail.com>
2019-11-07 12:35:33 +0200

    remove toolbar

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -162,7 +162,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    
 
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
