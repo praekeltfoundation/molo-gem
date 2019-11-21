acfd2c2c386b40a5562f4eb86f3f45579ba2ffec
Mitso Qalaba <mqalaba@gmail.com>
2019-11-21 11:18:32 +0200

    update pages

---
+++ gem/templates/chhaajaa/serviceworker.js
@@ -0,0 +1,33 @@
+
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+firebase.initializeApp({
+  'messagingSenderId': '158972131363'
+});
+
+const messaging = firebase.messaging();
+
+var CACHE = 'app-cache';
+
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(precache());
+});
+
+self.addEventListener('fetch', function(evt) {
+  evt.respondWith(fetch(evt.request).catch(function () {
+    return caches.open(CACHE).then(function(cache) {
+      return cache.match('/static/offline.html');
+    });
+  }));
+});
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      '/',
+      '/static/offline.html',
+      '/static/img/logos/chhaajaa-logo-old.png'
+    ]);
+  });
+}
