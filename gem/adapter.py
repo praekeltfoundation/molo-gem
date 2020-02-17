3f22a1796c66046d7d07560107510e37fde7476f
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-02-17 18:27:00 +0200

    update gem adaptor

--- gem/adapter.py
+++ gem/adapter.py
@@ -5,42 +5,42 @@ from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
-class StaffUserAdapter(DefaultAccountAdapter):
+class AdminAccessPermissionMixin:
+    def get_admin_perms(self):
+        wagtailadmin_content_type, created = ContentType.objects.get_or_create(
+            app_label='wagtailadmin',
+            model='admin'
+        )
+        admin_permission, created = Permission.objects.get_or_create(
+            content_type=wagtailadmin_content_type,
+            codename='access_admin',
+            name='Can access Wagtail admin'
+        )
+        return admin_permission
+
+
+class StaffUserAdapter(AdminAccessPermissionMixin, DefaultAccountAdapter):
     """ give users an is_staff default of true """
 
     def save_user(self, request, user, form, commit=True):
         user = super().save_user(request, user, form, commit=commit)
-        user.is_staff = True
-        wagtailadmin_content_type, created = ContentType.objects.get_or_create(
-            app_label='wagtailadmin',
-            model='admin'
-        )
-        admin_permission, created = Permission.objects.get_or_create(
-            content_type=wagtailadmin_content_type,
-            codename='access_admin',
-            name='Can access Wagtail admin'
-        )
-        user.user_permissions.add(admin_permission)
-        if commit:
-            user.save()
+        if not user.is_staff:
+            user.is_staff = True
+            user.user_permissions.add(self.get_admin_perms())
+            if commit:
+                user.save()
         return user
 
 
-class StaffUserSocialAdapter(DefaultSocialAccountAdapter):
+class StaffUserSocialAdapter(
+    AdminAccessPermissionMixin, DefaultSocialAccountAdapter
+):
     """ give users an is_staff default of true """
 
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form=form)
-        user.is_staff = True
-        wagtailadmin_content_type, created = ContentType.objects.get_or_create(
-            app_label='wagtailadmin',
-            model='admin'
-        )
-        admin_permission, created = Permission.objects.get_or_create(
-            content_type=wagtailadmin_content_type,
-            codename='access_admin',
-            name='Can access Wagtail admin'
-        )
-        user.user_permissions.add(admin_permission)
-        user.save()
+        if not user.is_staff:
+            user.is_staff = True
+            user.user_permissions.add(self.get_admin_perms())
+            user.save()
         return user
