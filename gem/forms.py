3312f676a475d399996ab541e573caf9a32d6a4f
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-02-27 14:32:45 +0200

    flake

--- gem/forms.py
+++ gem/forms.py
@@ -66,8 +66,8 @@ class PermissionGroupCheckboxSelect(CheckboxSelectMultiple):
     template_name = 'admin/permission_group_checkbox_select.html'
 
     def create_option(
-        self, name, value, label, selected, index,
-        subindex=None, attrs=None):
+            self, name, value, label, selected, index,
+            subindex=None, attrs=None):
 
         opt = super().create_option(
             name, value, label, selected, index,
