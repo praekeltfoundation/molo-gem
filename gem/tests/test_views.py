fe8a29e8e3b6f7391a203861dc630ff10b99ff61
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-04-20 16:28:51 +0200

    add chhaajaa test

--- gem/tests/test_views.py
+++ gem/tests/test_views.py
@@ -1,12 +1,15 @@
 from mock import patch
+from os.path import join
+from copy import deepcopy
 
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.sites.models import Site
 from django.urls import reverse
-from django.test import TestCase, Client, RequestFactory
-from django.test.utils import override_settings
+from django.conf import settings
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.test.utils import override_settings
+from django.contrib.contenttypes.models import ContentType
+from django.test import TestCase, Client, RequestFactory
 
 from molo.core.models import (
     Main, SectionIndexPage)
@@ -20,6 +23,11 @@ from molo.profiles.models import (
 
 from molo.commenting.forms import MoloCommentForm
 from molo.commenting.models import MoloComment
+
+from molo.forms.tests.base import create_molo_form_page
+from molo.forms.models import (
+    ArticlePageForms, FormsIndexPage,
+    MoloFormSubmission, MoloFormField)
 
 from gem.forms import GemRegistrationForm, GemEditProfileForm
 from gem.models import GemSettings, GemCommentReport, OIDCSettings
@@ -624,3 +632,117 @@ class TestCustomAuthenticationRequestView(TestCase, GemTestCaseMixin):
         self.assertEqual(view.get_extra_params(request), {
             'theme': 'springster', 'language': 'en'
         })
+
+
+class ChhaaJaaReactionQuestionsTest(TestCase, GemTestCaseMixin):
+    def setUp(self):
+        self.main = self.mk_main(
+            title='main1', slug='main1', path='00010002', url_path='/main1/')
+        self.section_index = SectionIndexPage.objects.child_of(
+            self.main).last()
+        self.yourmind = self.mk_section(
+            self.section_index, title='Your mind')
+        self.client = Client(HTTP_HOST=self.main.get_site().hostname)
+        self.user = User.objects.create_user(
+            username='tester',
+            email='tester@example.com',
+            password='tester')
+
+    def test_reaction_question(self):
+        template_settings = deepcopy(settings.TEMPLATES)
+        template_settings[0]['DIRS'] = [
+            join(settings.PROJECT_ROOT, 'templates', 'chhaajaa')
+        ]
+
+        with self.settings(TEMPLATES=template_settings):
+            self.client.force_login(self.user)
+            promote_date = timezone.now() + timezone.timedelta(days=-1)
+            demote_date = timezone.now() + timezone.timedelta(days=1)
+            article = self.mk_article(
+                self.yourmind,
+                feature_as_hero_article=True,
+                promote_date=promote_date,
+                demote_date=demote_date
+            )
+            forms_index = FormsIndexPage.objects.filter().first()
+            form = create_molo_form_page(
+                forms_index, display_form_directly=True)
+
+            form_field = MoloFormField.objects.create(
+                page=form, sort_order=1, label='q1', field_type='checkboxes',
+                required=True, page_break=False,
+                admin_label='q1', skip_logic=None, choices='a,b,c',
+            )
+            ArticlePageForms.objects.create(
+                page=article, form=form)
+
+            res = self.client.get('/')
+            self.assertEqual(res.status_code, 200)
+            self.assertContains(res, article.title)
+            self.assertTrue(form.title in str(res.content))
+
+            self.assertTrue(form_field.label in str(res.content))
+
+            data = {
+                form_field.admin_label: 'b'
+            }
+            MoloFormSubmission.objects.create(
+                page=form, article_page=article,
+                user=self.user, form_data=data
+            )
+
+            res = self.client.get('/')
+            self.assertEqual(res.status_code, 200)
+            self.assertContains(res, article.title)
+            self.assertTrue(form.title in str(res.content))
+            self.assertFalse(form_field.label in str(res.content))
+
+    def test_reaction_question_multi_submissions(self):
+        template_settings = deepcopy(settings.TEMPLATES)
+        template_settings[0]['DIRS'] = [
+            join(settings.PROJECT_ROOT, 'templates', 'chhaajaa')
+        ]
+
+        with self.settings(TEMPLATES=template_settings):
+            self.client.force_login(self.user)
+            promote_date = timezone.now() + timezone.timedelta(days=-1)
+            demote_date = timezone.now() + timezone.timedelta(days=1)
+            article = self.mk_article(
+                self.yourmind,
+                feature_as_hero_article=True,
+                promote_date=promote_date,
+                demote_date=demote_date
+            )
+            forms_index = FormsIndexPage.objects.filter().first()
+            form = create_molo_form_page(
+                forms_index, display_form_directly=True,
+                allow_multiple_submissions_per_user=True)
+
+            form_field = MoloFormField.objects.create(
+                page=form, sort_order=1, label='q1', field_type='checkboxes',
+                required=True, page_break=False,
+                admin_label='q1', skip_logic=None, choices='a,b,c',
+            )
+            ArticlePageForms.objects.create(
+                page=article, form=form)
+
+            res = self.client.get('/')
+            self.assertEqual(res.status_code, 200)
+            self.assertContains(res, article.title)
+            self.assertTrue(form.title in str(res.content))
+
+            self.assertTrue(form_field.label in str(res.content))
+
+            data = {
+                form_field.admin_label: 'b'
+            }
+            MoloFormSubmission.objects.create(
+                page=form, article_page=article,
+                user=self.user, form_data=data
+            )
+
+            res = self.client.get('/')
+            self.assertEqual(res.status_code, 200)
+            self.assertContains(res, article.title)
+            self.assertTrue(form.title in str(res.content))
+        
