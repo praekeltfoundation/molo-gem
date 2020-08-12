44fc004fcfc2a2489ebb9b56521aa4bd5fe4de25
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-08-12 12:31:14 +0200

    rm logical error still calling get_queryset twice

--- gem/admin_api.py
+++ gem/admin_api.py
@@ -1,42 +1,21 @@
 from django.conf.urls import url
 
 from wagtail.core import hooks
-from wagtail.core.models import Page
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.admin.api.endpoints import PagesAdminAPIEndpoint
-from wagtail.api.v2.utils import \
-    BadRequestError, parse_fields_parameter, \
-    filter_page_type, page_models_from_string
+from wagtail.api.v2.utils import BadRequestError, parse_fields_parameter
 
 
 class GemPagesAdminApi(PagesAdminAPIEndpoint):
     def get_queryset(self):
-        request = self.request
-
-        # Allow pages to be filtered to a specific type
-        try:
-            models = page_models_from_string(
-                request.GET.get('type', 'wagtailcore.Page'))
-        except (LookupError, ValueError):
-            raise BadRequestError("type doesn't exist")
-
-        if not models:
-            models = [Page]
-
-        if len(models) == 1:
-            print(request.GET, 'if', '*' * 100)
-            self.queryset = models[0].objects.exclude(depth=1).all()
-        else:
-            print(request.GET, 'else', '=' * 100)
-            self.queryset = filter_page_type(
-                Page.objects.exclude(depth=1).all(), models)
-
+        self.queryset = super(GemPagesAdminApi, self).get_queryset()
         return self.queryset
 
     def get_serializer_class(self):
         if self.action == 'listing_view':
             show_details = False
-            model = getattr(self, 'queryset', self.get_queryset()).model
+            model = self.queryset.model \
+                if self.queryset else self.get_queryset().model
         else:
             # Allow "detail_only" (eg parent) fields on detail view
             show_details = True
