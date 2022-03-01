c373a16fbc74e23b5664f6f0ba6d55483cb14b5b
Codie Roelf <codiebeulaine@gmail.com>
2022-03-01 19:30:37 +0200

    add comment user

--- gem/management/commands/download_comments.py
+++ gem/management/commands/download_comments.py
@@ -16,6 +16,9 @@ class Command(BaseCommand):
                 comment_data["parent_pk"] = comment.parent.slug
                 comment_data["parent_locale"] = comment.language.locales
             comment_data["wagtail_site"] = comment.wagtail_site.pk
+            if comment.user:
+                comment_data["username"] = comment.user.username
+                comment_data["user_is_staff"] = comment.user.is_staff
             comment_data["flag_count"] = comment.flags.all().count()
             comment_data["is_removed"] = comment.is_removed
             comment_data["comment"] = comment.comment
